bc3c6b7badfadafbf1fc32e74e5c4fad767e14f2
jha-Manisha <jhamanisha1106@gmail.com>
2022-05-04 16:35:06 +0530

    main

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+export function Navbar() {
+    return (
+        <div className="navbar">
+            <ul className="nav justify-content-end">
+  <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">About</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Link</a>
+  </li>
+
+</ul>
+        </div>
+    );
+}
